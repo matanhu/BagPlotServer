01fe7fecf6aecd2bec2fd42321875a8f71134817
MatanHu <MatanHuja@gmail.com>
2017-10-10 16:45:03 +0300

    Heroku Db

--- dbConnection/dbConnection.js
+++ dbConnection/dbConnection.js
@@ -1,10 +1,17 @@
 var mysql = require('mysql');
 
+// var connection = mysql.createConnection({
+//     host: '10.0.0.8',
+//     user: 'matan',
+//     password: '01111987',
+//     database: 'BagPlot'
+// });
+
 var connection = mysql.createConnection({
-    host: '10.0.0.8',
-    user: 'matan',
-    password: '01111987',
-    database: 'BagPlot'
+    host: 'eu-cdbr-west-01.cleardb.com',
+    user: 'bb348118811fba',
+    password: 'c9f3726afe20b3c',
+    database: 'heroku_19e127df84aa4a7'
 });
 
 function connectDB(query, values, callback) {
