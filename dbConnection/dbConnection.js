c9926579cee35e92832fd2362de159d3464fddfa
MatanHu <MatanHuja@gmail.com>
2017-10-15 00:28:37 +0300

    Add PROTOCOL_CONNECTION_LOST Heruko db

--- dbConnection/dbConnection.js
+++ dbConnection/dbConnection.js
@@ -1,38 +1,43 @@
 var mysql = require('mysql');
 
-// var connection = mysql.createConnection({
+// var db_config = {
 //     host: '10.0.0.8',
 //     user: 'matan',
 //     password: '01111987',
 //     database: 'BagPlot'
-// });
+// };
 
-var connection = mysql.createConnection({
+var db_config = {
     host: 'eu-cdbr-west-01.cleardb.com',
     user: 'bb348118811fba',
     password: 'c9f3726afe20b3c',
     database: 'heroku_19e127df84aa4a7'
-});
+};
 
-connection.on('error', function(err) {
-    console.log('db error', err);
-    if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
-      handleDisconnect();                         // lost due to either server restart, or a
-    } else {                                      // connnection idle timeout (the wait_timeout
-      throw err;                                  // server variable configures this)
-    }
-});
+var connection;
 
 function handleDisconnect() {
     connection = mysql.createConnection(db_config); // Recreate the connection, since
                                                     // the old one cannot be reused.
-
+  
     connection.connect(function(err) {              // The server is either down
-        if(err) {                                     // or restarting (takes a while sometimes).
+      if(err) {                                     // or restarting (takes a while sometimes).
         console.log('error when connecting to db:', err);
         setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
-        }                                     // to avoid a hot loop, and to allow our node script to
-});
+      }                                     // to avoid a hot loop, and to allow our node script to
+    });                                     // process asynchronous requests in the meantime.
+                                            // If you're also serving http, display a 503 error.
+    connection.on('error', function(err) {
+      console.log('db error', err);
+      if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+        handleDisconnect();                         // lost due to either server restart, or a
+      } else {                                      // connnection idle timeout (the wait_timeout
+        throw err;                                  // server variable configures this)
+      }
+    });
+}
+
+handleDisconnect();
 
 function connectDB(query, values, callback) {
     try {
