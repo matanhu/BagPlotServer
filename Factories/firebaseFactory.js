df28a85eb7b3e4ab777b3f34f9d7d3c5da84d945
MatanHu <MatanHuja@gmail.com>
2017-11-23 01:22:26 +0200

    config Firebase Admin

--- Factories/firebaseFactory.js
+++ Factories/firebaseFactory.js
@@ -1,15 +1,15 @@
-/*
+
 var admin = require("firebase-admin");
 
 var serviceAccount = require("../firebaseFiles/serviceAccountKey.json");
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://bagplot-b2914.firebaseio.com"
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://bagplot-b2914.firebaseio.com"
 });
 
 var FirebaseFactory = {
-    createUserByEmail: function(signupModel) {
+    createUserByEmail: function (signupModel) {
         return admin.auth().createUser({
             email: signupModel.email,
             emailVerified: false,
@@ -18,41 +18,45 @@ var FirebaseFactory = {
             displayName: signupModel.firstName + '' + signupModel.lastName,
             // photoURL: "http://www.example.com/12345678/photo.png",
             disabled: false
-          });
+        });
+    },
+
+    verifyIdToken: function (idToken) {
+        return admin.auth().verifyIdToken(idToken);
     }
 }
 
-module.exports = FirebaseFactory; 
-*/
+module.exports = FirebaseFactory;
 
-var firebase = require('firebase');
 
-firebase.initializeApp({
-    apiKey: "AIzaSyC0LA_uWVK0YiGLG_FVNWZc_T1Fw9P6zRA",
-    authDomain: "bagplot-b2914.firebaseapp.com",
-    databaseURL: "https://bagplot-b2914.firebaseio.com",
-    projectId: "bagplot-b2914",
-    storageBucket: "bagplot-b2914.appspot.com",
-    messagingSenderId: "577810091219"
-  });
+// var firebase = require('firebase');
 
-  var FirebaseFactory = {
-    createUserByEmail: function(signupModel) {
-        return firebase.auth().createUserWithEmailAndPassword(signupModel.email, signupModel.password);
-    },
-    signinUserByEmail: function(signinModel) {
-        return firebase.auth().signInWithEmailAndPassword(signinModel.email, signinModel.password);
-    },
-    updateProfile: function(signupModel) {
-        var user = firebase.auth().currentUser;
-        if(user) {
-            return user.updateProfile({
-                displayName: signupModel.firstName + ' ' + signupModel.lastName,
-            });
-        }
-    }
-}
+// firebase.initializeApp({
+//     apiKey: "AIzaSyC0LA_uWVK0YiGLG_FVNWZc_T1Fw9P6zRA",
+//     authDomain: "bagplot-b2914.firebaseapp.com",
+//     databaseURL: "https://bagplot-b2914.firebaseio.com",
+//     projectId: "bagplot-b2914",
+//     storageBucket: "bagplot-b2914.appspot.com",
+//     messagingSenderId: "577810091219"
+//   });
 
-module.exports = FirebaseFactory; 
+//   var FirebaseFactory = {
+//     createUserByEmail: function(signupModel) {
+//         return firebase.auth().createUserWithEmailAndPassword(signupModel.email, signupModel.password);
+//     },
+//     signinUserByEmail: function(signinModel) {
+//         return firebase.auth().signInWithEmailAndPassword(signinModel.email, signinModel.password);
+//     },
+//     updateProfile: function(signupModel) {
+//         var user = firebase.auth().currentUser;
+//         if(user) {
+//             return user.updateProfile({
+//                 displayName: signupModel.firstName + ' ' + signupModel.lastName,
+//             });
+//         }
+//     }
+// }
+
+// module.exports = FirebaseFactory; 
 
 
--- server.js
+++ server.js
@@ -6,12 +6,12 @@ var app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
-  });
+});
 
 
 var customersFactory = require('./Factories/customersFactory');
@@ -22,96 +22,96 @@ var projectItemFactory = require('./Factories/projectItemFactory');
 var firebaseFactory = require('./Factories/firebaseFactory');
 var userFactory = require('./Factories/userFactory');
 
-var port=Number(process.env.PORT || 3000);
+var port = Number(process.env.PORT || 3000);
 
-app.get('/api/getAllCustomers', function(req, res) {
-    customersFactory.getAllCustomers(function(customerList) {
+app.get('/api/getAllCustomers', function (req, res) {
+    customersFactory.getAllCustomers(function (customerList) {
         res.send(customerList);
     });
 });
 
-app.post('/api/getCustomersByName', function(req,res) {
+app.post('/api/getCustomersByName', function (req, res) {
     var customer_name = req.body.customer_name;
-    customersFactory.getCustomersByName(customer_name, function(customersList) {
+    customersFactory.getCustomersByName(customer_name, function (customersList) {
         res.send(customersList);
     });
 });
 
-app.post('/api/createCustomer', function(req,res) {
+app.post('/api/createCustomer', function (req, res) {
     var customer_name = req.body.customer_name;
-    customersFactory.createCustomer(customer_name, function(customer) {
+    customersFactory.createCustomer(customer_name, function (customer) {
         res.send(customer);
     });
 });
 
-app.get('/api/getCountOfProjects', function(req, res) {
-    projectsFactory.getCountOfProjects(function(projectCount) {
+app.get('/api/getCountOfProjects', function (req, res) {
+    projectsFactory.getCountOfProjects(function (projectCount) {
         res.send(projectCount);
     });
 });
 
-app.get('/api/getAllProjects', function(req, res) {
-    projectsFactory.getAllProjects(function(projectList) {
+app.get('/api/getAllProjects', function (req, res) {
+    projectsFactory.getAllProjects(function (projectList) {
         res.send(projectList);
     });
 });
 
-app.get('/api/searchProject/:searchText?', function(req, res) {
+app.get('/api/searchProject/:searchText?', function (req, res) {
     var searchText = '';
-    if(req.param('searchText')) {
+    if (req.param('searchText')) {
         searchText = req.param('searchText');
     }
-    projectsFactory.searchProject(searchText, function(projectList) {
+    projectsFactory.searchProject(searchText, function (projectList) {
         res.send(projectList);
     });
 })
 
-app.get('/api/getProjectByIdClient/:id', function(req, res) {
+app.get('/api/getProjectByIdClient/:id', function (req, res) {
     var projectId = req.param('id');
-    projectsFactory.getProjectByIdClient(projectId, function(projectRes) {
+    projectsFactory.getProjectByIdClient(projectId, function (projectRes) {
         res.send(projectRes);
     });
 });
 
-app.delete('/api/deleteProjectById/:id', function(req, res) {
+app.delete('/api/deleteProjectById/:id', function (req, res) {
     var projectId = req.param('id');
-    projectsFactory.deleteProjectById(projectId, function(projectRes) {
+    projectsFactory.deleteProjectById(projectId, function (projectRes) {
         res.send(projectRes);
     });
 });
 
-app.post('/api/createProject', function(req,res) {
+app.post('/api/createProject', function (req, res) {
     var project = req.body;
-    projectsFactory.createProject(project, function(project) {
+    projectsFactory.createProject(project, function (project) {
         res.send(project);
     });
 });
 
-app.put('/api/updateProject', function(req,res) {
+app.put('/api/updateProject', function (req, res) {
     var project = req.body;
-    projectsFactory.updateProject(project, function(project) {
+    projectsFactory.updateProject(project, function (project) {
         res.send(project);
     });
 });
 
-app.post('/api/createContact', function(req, res) {
+app.post('/api/createContact', function (req, res) {
     var contact = req.body;
-    contactFactory.createContact(contact, function(contact) {
+    contactFactory.createContact(contact, function (contact) {
         res.send(contact);
     });
 });
 
 // app.post('/createDocx/:projectId', function(req, res) {
-app.post('/api/createDocx/', function(req, res) {
+app.post('/api/createDocx/', function (req, res) {
     var project = req.body.project;
     var emailTo = req.body.emailTo;
     var hostUrl = req.get('host');
     console.log('req.get("host"): ' + req.get('host'));
-    console.log('req.originalUrl: ' + req.originalUrl );
+    console.log('req.originalUrl: ' + req.originalUrl);
     // var project = {
     //     id: req.param('projectId')
     // }
-    documentFactory.createDocx(project, emailTo, hostUrl, function(file) {
+    documentFactory.createDocx(project, emailTo, hostUrl, function (file) {
         console.log(file);
         // docx.generate(res);
         // res.setHeader('Content-disposition', 'attachment; filename=BagPlot');
@@ -123,70 +123,73 @@ app.post('/api/createDocx/', function(req, res) {
     });
 });
 
-app.get('/api/dowloadDocx/:projectId/:filename', function(req,res) {
+app.get('/api/dowloadDocx/:projectId/:filename', function (req, res) {
     var projectId = req.param('projectId');
     var filename = req.param('filename');
-    documentFactory.dowloadDocx(projectId, filename, function(file) {
+    documentFactory.dowloadDocx(projectId, filename, function (file) {
         // res.setHeader('Content-disposition', 'attachment; filename=' + filename);
         res.download(file.tempFile);
     });
 });
 
-app.post('/api/createProjecrItem/', function(req,res) {
+app.post('/api/createProjecrItem/', function (req, res) {
     var projectItem = req.body;
-    projectItemFactory.createProjecrItem(projectItem, function(projectItemRes){
+    projectItemFactory.createProjecrItem(projectItem, function (projectItemRes) {
         res.send(projectItemRes);
     });
 });
 
-app.get('/api/getProjectItemById/:projectItemId', function(req, res) {
+app.get('/api/getProjectItemById/:projectItemId', function (req, res) {
     var projectItemId = req.param('projectItemId');
-    projectItemFactory.getProjectItemById(projectItemId, function(projectItemRes) {
+    projectItemFactory.getProjectItemById(projectItemId, function (projectItemRes) {
         res.send(projectItemRes);
     });
 });
 
-app.put('/api/updateProjectItem/', function(req, res) {
+app.put('/api/updateProjectItem/', function (req, res) {
     var projectItem = req.body;
-    projectItemFactory.updateProjectItem(projectItem, function(projectItemRes) {
+    projectItemFactory.updateProjectItem(projectItem, function (projectItemRes) {
         res.send(projectItemRes);
     });
 });
 
-app.post('/api/signup', function(req, res) {
+app.post('/api/signup', function (req, res) {
     var signupModel = req.body;
-    firebaseFactory.createUserByEmail(signupModel)
-        .then(function(userRecord) {
-            // See the UserRecord reference doc for the contents of userRecord.
-            console.log("Successfully created new user:", userRecord.uid);
-            signupModel.uid = userRecord.uid;
-            firebaseFactory.updateProfile(signupModel).then(function() {
-                userFactory.createUser(signupModel, function(userRes) {
-                    res.send(userRes);
-                })
-            })
-            .catch(function(error) {
-                console.log("Error firebaseFactory updateProfile user:", error);    
-            });
-        })
-        .catch(function(error) {
-            console.log("Error creating new user:", error);
+    userFactory.createUser(signupModel, function (userRes) {
+        res.send(userRes);
+    });
+});
+
+app.post('/api/signin', function (req, res) {
+    var token = req.body;
+    firebaseFactory.verifyIdToken(token.token)
+        .then(function (decodedToken) {
+            var uid = decodedToken.uid;
+            var signinRes = {
+                isSuccess: true
+            }
+            res.send(signinRes);
+        }).catch(function (error) {
+            var signinRes = {
+                isSuccess: false,
+                error: error
+            }
+            res.send(signinRes);
         });
 });
 
-app.post('/api/signin', function(req, res) {
-    var signinModel = req.body;
-    firebaseFactory.signinUserByEmail(signinModel)
-        .then(function(userRecord) {
-            // See the UserRecord reference doc for the contents of userRecord.
-            console.log("Successfully signin user:", userRecord.uid);
-            signinModel.uid = userRecord.uid;
-            res.send(userRecord);
-        })
-        .catch(function(error) {
-            console.log("Error signin user:", error);
-        });
-});
+// app.post('/api/signin', function (req, res) {
+//     var signinModel = req.body;
+//     firebaseFactory.signinUserByEmail(signinModel)
+//         .then(function (userRecord) {
+//             // See the UserRecord reference doc for the contents of userRecord.
+//             console.log("Successfully signin user:", userRecord.uid);
+//             res.send(userRecord);
+//         })
+//         .catch(function (error) {
+//             console.log("Error signin user:", error);
+//         });
+// });
 console.log(port);
 app.listen(port);
 
